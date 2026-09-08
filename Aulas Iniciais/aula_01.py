2f01bea450de2b578351ba9360912a0160c8c24e
DiogoRPaiano <diogorpaeyt@gmail.com>
2026-09-08 16:01:36 -0300

    First Lesson

---
+++ Aulas Iniciais/aula_01.py
@@ -0,0 +1,11 @@
+# print("Ola, mundo!")
+# print("Meu nome e Koqui.")
+# print("Estou aprendendo Python.")
+
+nome = input("Insira seu nome: ")
+idade = input("Insira sua idade: ")
+cidade = input("Insira a cidade onde mora: ")
+programa = input("Insira um programa que deseja criar: ")
+
+#print("Prazer " + nome + ", você tem " + idade + " anos e mora na cidade " + cidade + " e deseja criar um programa sobre " + programa + ".")
+print(f"Prazer, {nome}! Você tem {idade} anos e mora na cidade {cidade} e deseja criar um programa sobre {programa}")
